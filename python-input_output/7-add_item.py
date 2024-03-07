1a549db04af31bcb1b6645716b61392402a227a9
dianKoliva <dianpretty01@gmail.com>
2024-03-07 15:50:29 +0200

    neww

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list."""
+"""
+Script that adds all arguments to a Python list and saves them to a JSON file.
+"""
 
 import sys
+from typing import List
+from os.path import exists
+from json import dump, load
+
+
+def save_to_json_file(my_obj: List, filename: str):
+    """ Saves an object to a JSON file """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        dump(my_obj, f)
+
+
+def load_from_json_file(filename: str) -> List:
+    """ Loads an object from a JSON file """
+    if exists(filename):
+        with open(filename, mode='r', encoding='utf-8') as f:
+            return load(f)
+    return []
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+    
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
