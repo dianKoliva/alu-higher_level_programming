639b56059ec21997a4d1c7abd3bdd653c059c545
dianKoliva <dianpretty01@gmail.com>
2024-03-11 12:45:43 +0200

    new llll

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 def add_arguments_to_list(arguments: List[str]) -> None:
-    
+
     """Add all arguments to a Python list and save them to a file."""
     try:
         if exists('add_item.json'):
@@ -21,4 +21,3 @@ def add_arguments_to_list(arguments: List[str]) -> None:
 
 if __name__ == "__main__":
     add_arguments_to_list(argv[1:])
-
